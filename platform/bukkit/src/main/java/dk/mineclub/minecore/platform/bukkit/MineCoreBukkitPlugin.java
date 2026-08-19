3383d1097aa4bda4ad5bf45ccd6ef20cb98ca0bc
Itz_Hoffe <mhoff770@gmail.com>
2026-08-19 09:28:37 +0200

    Revert "Staff panel"
    This reverts commit c1a62785508014dd96faae97c80de95702d00aa7.

--- platform/bukkit/src/main/java/dk/mineclub/minecore/platform/bukkit/MineCoreBukkitPlugin.java
+++ platform/bukkit/src/main/java/dk/mineclub/minecore/platform/bukkit/MineCoreBukkitPlugin.java
@@ -3,6 +3,7 @@ package dk.mineclub.minecore.platform.bukkit;
 import dk.mineclub.minecore.platform.common.BaseMineCorePlugin;
 import dk.mineclub.minecore.platform.common.hooks.SkriptHookDownloader;
 
+/** Main entry point for the Bukkit platform module. */
 public class MineCoreBukkitPlugin extends BaseMineCorePlugin {
     @Override
     protected String platformName() {
@@ -12,17 +13,5 @@ public class MineCoreBukkitPlugin extends BaseMineCorePlugin {
     @Override
     protected SkriptHookDownloader createSkriptDownloader() {
         return new BukkitSkriptHookDownloader(this);
-    }
-
-    @Override
-    public void onEnable() {
-        super.onEnable();
-
-        if (getApi() == null) {
-            return;
-        }
-
-        getServer().getPluginManager().registerEvents(new StaffFeedListener(getApi()), this);
-        StaffCommandBridge.register(this, getApi());
     }
 }
